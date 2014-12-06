d0711f58861ebe3aed1db09a39b6a53a35e34cd9
cow12331 <cow123311@gmail.com>
2014-12-06 15:17:56 -0500

    checkPW
    checkPW

--- src/com/musictour/CheckPassword.java
+++ src/com/musictour/CheckPassword.java
@@ -1,21 +1,26 @@
 package com.musictour;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+
+import com.musictour.dbManager.DBManager;
+
 /**
- * Servlet implementation class Registration
+ * Servlet implementation class CheckPassword
  */
-public class Registration extends HttpServlet {
+public class CheckPassword extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Registration() {
+    public CheckPassword() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -25,15 +30,24 @@ public class Registration extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().write("good");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().write("good");
+		int flag = Integer.parseInt(request.getParameter("flag"));
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		DBManager ma = new DBManager();
+		ma.getDirver();
+		ma.connect();
+		int num = ma.checkPassWorld(name, password, flag);
+		ma.shutdown();
+		
+		JSONObject obj = new JSONObject();
+		obj.put("status", new String(num > 0 ? "success" : "fail"));
+		response.getWriter().write(obj.toJSONString());
 	}
 
 }
